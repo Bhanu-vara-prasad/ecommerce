6aed4c5402480371d8148c435bfc4c4550cf3045
Bhanu-vara-prasad <bhanuvaraprasadgudupu@gmail.com>
2025-07-17 22:36:06 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  server:{port:5173}
+  plugins: [react()],
+  server: {
+    port: 5173
+  },
+  build: {
+    outDir: 'dist' // Vercel expects this by default
+  }
 })
